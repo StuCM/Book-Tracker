d1168f877feb6ae6401ebe232f2fd8129a1f3aa8
Stuart Marshall <stucmarshall@gmail.com>
2023-07-24 22:15:48 +0100

    add books to the bookshelf

--- main.js
+++ main.js
@@ -1,49 +1,66 @@
 let myLibrary = [];
 
-function Book(title, author, published, pages, rating, read) {
+function Book(title, author, published, pages, rating, read, cover) {
     this.title = title,
     this.author = author,
     this.published = published,
     this.pages = pages,
     this.rating = rating,
     this.read = read;
+    this.cover = cover;
 }
 
 async function addBookToLibrary() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
     let rating = document.getElementsByName('stars');
     for(let star of rating) {
         if(star.checked) rating = star.value;
     }
-    let bookData = await getBookData(title, author);
-    console.log("book data", bookData)
+    let bookData = await getBookData(title.value, author.value);
+    let bookCover = await getBookCover(bookData.cover_i)
     let newBook = new Book(
         bookData.title, 
         bookData.author_name[0], 
         bookData.publish_date[0], 
         bookData.number_of_pages_median,
         rating,
-        false
+        false,
+        bookCover
         )
-    console.log(bookData)
-    getBookCover(bookData.cover_i)
+    console.log("new book", newBook)    
     let alreadyAdded = myLibrary.some(entry => entry.title === newBook.title && entry.author === newBook.author);
     console.log(alreadyAdded)
     if(!alreadyAdded) {
         myLibrary.push(newBook);
+        let index = myLibrary.indexOf(newBook);
+        addBookToGrid(index);
         console.log(myLibrary)
         this.toggleBookModal();
+        title.value = "";
+        author.value = "";
+        rating = 3;
     }
     else{ console.log("This book has already been added") };
 }
 
+function addBookToGrid(index) {
+    const temp = document.getElementsByTagName('template')[0];
+    const bookTemp = temp.content.querySelector('div');
+    const clonedTemplate = document.importNode(bookTemp, true);
+    console.log(clonedTemplate)
+    const bookCoverTemp = clonedTemplate.querySelector('#cover');
+    const book = myLibrary[index];
+    bookCoverTemp.src = book.cover;
+    bookCoverTemp.alt = book.title;
+
+    const bookshelf = document.getElementById("bookshelf");
+    bookshelf.appendChild(clonedTemplate);
+}
+
 async function getBookCover(id){
     let response = await fetch("https://covers.openlibrary.org/b/id/" + id + "-M.jpg");
-    let cover = document.getElementById("cover")
-    cover.setAttribute("src", response.url);
-
-    console.log(response, response.url)
+    return response.url;
 }
 
 async function getBookData(title, author) {
